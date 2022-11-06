878f280ba45c2c4b79fb078321aabd6c70d8fa3a
Lahoucine Hamsek <49314721+RIIDLEY@users.noreply.github.com>
2022-11-06 20:42:19 +0100

    update generation map

--- classes.py
+++ classes.py
@@ -1,13 +1,15 @@
 import pygame
 from constant import *
+from random import randrange
+
 
 class Niveau:
     """class de l'ffichage du terrain"""
 
     def __init__(self):
         """initialisation des variables de la class"""
-        self.x = 20
-        self.y = 20
+        self.x = 40
+        self.y = 40
         self.screen = pygame.display.set_mode((1280, 720))
         self.structure = grid
         print("Init Niveau")
@@ -18,7 +20,15 @@ class Niveau:
             for x, c in enumerate(line):
                 if (c==0):#Sol
                     print("Sol")
-                    self.screen.blit(image_sol, (x * 40, y * 40))
+                    if(randrange(0, 100) < 70):
+                        print("Box")
+                        self.screen.blit(image_box, (x*self.x, y*self.y))
+                    else:
+                        print("Sol")
+                        self.screen.blit(image_sol, (x * self.x, y * self.y))
+                if(c==9 or c==8):#Mur
+                    print("Sol")
+                    self.screen.blit(image_sol, (x * self.x, y * self.y))
                 if (c==1):#Murs
                     print("Murs")
-                    self.screen.blit(image_brick, (x * 40, y * 40))
+                    self.screen.blit(image_brick, (x * self.x, y * self.y))
